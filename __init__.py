0f8e0de8e29167c58892932900dd685991de2eea
Avinash Vyas <avinash.vyas@nokia.com>
2018-05-15 12:26:47 -0400

    initial commit for padatious ppt controller

--- __init__.py
+++ __init__.py
@@ -5,11 +5,27 @@ from mycroft import MycroftSkill, intent_handler
 class PptControllerUsingPadatiousSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+	self.url = "http://135.222.162.94:8001"
+	self.file_opened = False
 
-    @intent_handler(IntentBuilder().require('PptControllerUsingPadatious'))
+    @intent_file_handler('ppt.controller.intent')
     def handle_ppt_controller_using_padatious(self, message):
         self.speak_dialog('ppt.controller.using.padatious')
 
+    @intent_file_handler('ppt.open.intent')
+    def handle_ppt_open(self, message):
+	filename = message.data.get("filename")
+	self.enclosure.mouth_text("Nova opening file " + filename)
+	self.file_opened = True;
+	# Send a rest request
+	param = {'filename':filename}
+	self.enclosure.mouth_text("Sending request to " + self.url);
+	#response = requests.get(self.url, param)
+	resp = {'filename' : filename}
+	#if response.status_code == requests.codes.ok:
+        	self.speak_dialog('ppt.open', data=resp)
+	#else: 
+	#	self.speak_dialog('ppt.filenotfound')
 
 def create_skill():
     return PptControllerUsingPadatiousSkill()
