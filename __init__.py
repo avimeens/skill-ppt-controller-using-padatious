74f788068f97aea2e2b7b7db190612de00ceda4f
Avinash Vyas <avinash.vyas@nokia.com>
2018-05-15 15:23:37 -0400

    Add support for closing/ending presentation

--- __init__.py
+++ __init__.py
@@ -21,11 +21,28 @@ class PptControllerUsingPadatiousSkill(MycroftSkill):
 	param = {'filename':filename}
 	self.enclosure.mouth_text("Sending request to " + self.url);
 	#response = requests.get(self.url, param)
+	response.status_code = requests.codes.ok
 	resp = {'filename' : filename}
-	#if response.status_code == requests.codes.ok:
-        self.speak_dialog('ppt.open', data=resp)
-	#else: 
-	#	self.speak_dialog('ppt.filenotfound')
+	if response.status_code == requests.codes.ok:
+        	self.speak_dialog('ppt.open', data=resp)
+	else: 
+		self.speak_dialog('ppt.filenotfound')
+
+	
+    @intent_file_handler('ppt.close.intent')
+    def handle_ppt_close(self, message):
+	# Send a rest request
+	if self.file_opened: 
+		purl = self.url + "/close"
+		self.enclosure.mouth_text("Sending request to " + purl);
+		#response = requests.get(purl)
+		response.status_code = requests.codes.ok
+		if response.status_code == requests.codes.ok:
+        		self.speak_dialog('ppt.close')
+		else: 
+			self.speak_dialog('ppt.filenotfound')
+	else: 
+		self.speak_dialog('ppt.filenotopen')
 
 def create_skill():
     return PptControllerUsingPadatiousSkill()
